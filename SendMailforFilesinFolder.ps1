0bf6ec130720cfd6780cf01f8bbfdffe9e8b8c60
PhaNDoMs <phandoms.business@gmail.com>
2018-11-02 12:54:29 +0100

    Create SendMailforFilesinFolder.ps1

---
+++ SendMailforFilesinFolder.ps1
@@ -0,0 +1,26 @@
+function sendMail ($mailbody){ 
+     #SMTP server name
+     $smtpServer = "mail.server.com"
+
+     #Creating a Mail object
+     $msg = new-object Net.Mail.MailMessage
+
+     #Creating SMTP server object
+     $smtp = new-object Net.Mail.SmtpClient($smtpServer)
+
+     #Email structure
+     $msg.From = "Sender@domain.com"
+     $msg.ReplyTo = "Sender@domain.com"
+     $msg.To.Add("receiver@domain.com")
+     $msg.subject = "Automated Log Email"
+     $msg.body = $mailbody
+
+     #Sending email
+     $smtp.Send($msg)
+}
+$path = "C:\Files"
+Get-ChildItem $path -Filter *.txt |
+Foreach-Object {
+    $fileContent = (Get-Content $_.FullName | Out-String)    
+    sendMail $fileContent
+}
